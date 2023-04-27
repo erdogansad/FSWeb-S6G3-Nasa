31e0d2b656dc72da1c5016b4203bfb1fb5a950f3
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-04-27 15:57:11 +0300

    initial loading component & useEffect

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./Home.css";
 
-const Home = props => {
-  const {data} = props;
+const Home = ({data}) => {
+  const [apodData, setApodData] = useState({});
 
-  let date = data[0].date;
-  date = date.split("-");
-  date = `${date[2]}.${date[1]}.${date[0]}`
+  useEffect(() => {
+    if(data !== undefined){
+      data.date = data.date.split("-");
+      data.date = `(${data.date[2]}.${data.date[1]}.${data.date})`
+      setTimeout(() => {
+        setApodData(data);
+      }, 1500);
+    }
+  }, [data]);     
+
+  if (!apodData.hdurl){
+    return (
+      <section className="container">
+        <div className="apod-container">
+          <div className="apod-content">
+            <h2 className="loading-text">Yükleniyor...</h2>
+          </div>
+        </div>
+      </section>
+    );
+  }
 
   return (
       <section className="container">
         <div className="apod-container">
-          <img className="apod-background" src={data[0].hdurl} alt=""/>
+          <img className="apod-background" src={apodData.hdurl} alt=""/>
           <div className="apod-content">
-            <img className="apod-image" src={data[0].url} alt=""/>
+            <img className="apod-image" src={apodData.url} alt=""/>
             <div className="apod-text">
-              <h2 className="apod-title">{data[0].title}</h2>
-              <h3 className="apod-copyright">{data[0].copyright} <span className="apod-date">({date})</span></h3>
-              <p className="apod-explanation">{data[0].explanation}</p>
+              <h2 className="apod-title">{apodData.title}</h2>
+              <h3 className="apod-copyright">{apodData.copyright} <span className="apod-date">{apodData.date}</span></h3>
+              <p className="apod-explanation">{apodData.explanation}</p>
             </div>
           </div>
         </div>
