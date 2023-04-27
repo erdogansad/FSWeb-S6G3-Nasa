9f3dab2c951b2c4d4fcbd7ba1d813d304ef45616
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-04-27 03:46:38 +0300

    date fix

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -3,7 +3,11 @@ import "./Home.css";
 
 const Home = props => {
   const {data} = props;
-  console.log(data[0])
+
+  let date = data[0].date;
+  date = date.split("-");
+  date = `${date[2]}.${date[1]}.${date[0]}`
+
   return (
       <section className="container">
         <div className="apod-container">
@@ -12,7 +16,7 @@ const Home = props => {
             <img className="apod-image" src={data[0].url} alt=""/>
             <div className="apod-text">
               <h2 className="apod-title">{data[0].title}</h2>
-              <h3 className="apod-copyright">{data[0].copyright} <span className="apod-date">({data[0].date})</span></h3>
+              <h3 className="apod-copyright">{data[0].copyright} <span className="apod-date">({date})</span></h3>
               <p className="apod-explanation">{data[0].explanation}</p>
             </div>
           </div>
