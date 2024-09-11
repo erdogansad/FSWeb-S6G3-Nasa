2f9cae6760a7cdae14e7897fd372030cc87823ba
erdogansad <erdogan.sad@windowslive.com>
2024-09-11 18:12:24 +0300

    Refactor Error page component to handle dynamic error messages

---
+++ src/pages/Error.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+const NotFound = ({ error }) => {
+  const location = useLocation();
+  const [text, setText] = React.useState({
+    title: "404",
+    subtitle: "Sayfa bulunamadı",
+  });
+
+  React.useEffect(() => {
+    if (error) {
+      setText({
+        title: error.status,
+        subtitle: error.message,
+      });
+    }
+
+    if (location.state && location.state.error) {
+      setText({
+        title: location.state.error.status,
+        subtitle: location.state.error.message,
+      });
+    }
+
+    return () => {
+      setText({
+        title: "404",
+        subtitle: "Sayfa bulunamadı",
+      });
+    };
+  }, [error, location]);
+
+  return (
+    <section className="bg-stone-900 h-full flex flex-col gap-4 justify-center items-center">
+      <h1 className="font-inter font-bold text-4xl text-stone-300">{text.title}</h1>
+      <h2 className="font-inter font-bold text-3xl text-stone-300">{text.subtitle}</h2>
+    </section>
+  );
+};
+
+export default NotFound;
