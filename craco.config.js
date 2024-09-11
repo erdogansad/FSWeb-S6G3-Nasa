2d96c3662508b85b98ba997497697fae82b4741c
erdogansad <erdogan.sad@windowslive.com>
2024-09-11 18:10:35 +0300

    Add craco.config.js for webpack configuration

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  reactScriptsVersion: "react-scripts",
+  webpack: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+    },
+    mode: "extends",
+  },
+};
